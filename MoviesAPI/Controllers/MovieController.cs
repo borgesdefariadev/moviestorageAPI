a90749c215cbf2b1da6a0d5b58d9aa7275827d9c
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-09-02 16:07:35 -0300

    Add Movie model and controller
    Introduces the Movie model and a MovieController with an endpoint to add movies. Provides a static list of sample movies and a POST method for adding new entries.

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Models;
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+
+    private static List<Movie> movies = new()
+    {
+        new Movie { Id = 1, Title = "Inception", Genre = "Drama", Director = "Christopher Nolan", ReleaseYear = 2010 },
+        new Movie { Id = 2, Title = "The Matrix", Genre = "Drama", Director = "The Wachowskis", ReleaseYear = 1999 },
+        new Movie { Id = 3, Title = "Interstellar", Genre = "Sci-Fi", Director = "Christopher Nolan", ReleaseYear = 2014 }
+    };
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        // Implementation for adding a movie
+        movies.Add(movie);
+    }
+}

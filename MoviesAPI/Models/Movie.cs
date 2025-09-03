2522c8c49657a211ac70d88258c902dadfda5c47
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-09-03 13:21:13 -0300

    Add validation attributes to Movie model
    Introduced data annotation attributes to the Movie model for required fields, maximum lengths, and input format validation. Also updated launchSettings.json to disable automatic browser launch for the HTTP profile.

--- MoviesAPI/Models/Movie.cs
+++ MoviesAPI/Models/Movie.cs
@@ -1,13 +1,25 @@
-﻿using System.Security.Principal;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 
 namespace MoviesAPI.Models;
 
 public class Movie
 {
+    [Required]
+    [Key]    
     public int Id { get; set; }
+    [Required(ErrorMessage = "Title is Required.")]
+    [MaxLength(50, ErrorMessage = "Title cannot be more than 50 characters")]
     public string? Title { get; set; }
+    [Required]
+    [MaxLength(30, ErrorMessage = "Genre cannot be more than 30 characters")]
     public string? Genre { get; set; }
+    [Required]
+    [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Use Characters only")]
     public string? Director { get; set; }
+    [Required]
+    //[MaxLength(4, ErrorMessage = "Release year cannot be more than 4 characters")]
     public int ReleaseYear { get; set; }
 
 }

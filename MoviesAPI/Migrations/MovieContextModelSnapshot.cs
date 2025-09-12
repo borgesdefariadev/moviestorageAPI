2825fec0e2f3ce6fcd4ad189e811442aa0258977
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-09-12 12:37:49 -0300

    Refactor migration files to use file-scoped namespaces
    Updated CreateMovieTable and MovieContextModelSnapshot migration files to use file-scoped namespaces and simplified class structure for improved readability and consistency with modern C# conventions.

--- MoviesAPI/Migrations/MovieContextModelSnapshot.cs
+++ MoviesAPI/Migrations/MovieContextModelSnapshot.cs
@@ -6,46 +6,45 @@ using MoviesAPI.Data;
 
 #nullable disable
 
-namespace MoviesAPI.Migrations
+namespace MoviesAPI.Migrations;
+
+[DbContext(typeof(MovieContext))]
+partial class MovieContextModelSnapshot : ModelSnapshot
 {
-    [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.10")
-                .HasAnnotation("Relational:MaxIdentifierLength", 64);
+        modelBuilder
+            .HasAnnotation("ProductVersion", "6.0.10")
+            .HasAnnotation("Relational:MaxIdentifierLength", 64);
 
-            modelBuilder.Entity("MoviesAPI.Models.Movie", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
+        modelBuilder.Entity("MoviesAPI.Models.Movie", b =>
+            {
+                b.Property<int>("Id")
+                    .ValueGeneratedOnAdd()
+                    .HasColumnType("int");
 
-                    b.Property<string>("Director")
-                        .IsRequired()
-                        .HasColumnType("longtext");
+                b.Property<string>("Director")
+                    .IsRequired()
+                    .HasColumnType("longtext");
 
-                    b.Property<string>("Genre")
-                        .IsRequired()
-                        .HasMaxLength(30)
-                        .HasColumnType("varchar(30)");
+                b.Property<string>("Genre")
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .HasColumnType("varchar(30)");
 
-                    b.Property<int>("ReleaseYear")
-                        .HasColumnType("int");
+                b.Property<int>("ReleaseYear")
+                    .HasColumnType("int");
 
-                    b.Property<string>("Title")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("varchar(50)");
+                b.Property<string>("Title")
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnType("varchar(50)");
 
-                    b.HasKey("Id");
+                b.HasKey("Id");
 
-                    b.ToTable("Movies");
-                });
+                b.ToTable("Movies");
+            });
 #pragma warning restore 612, 618
-        }
     }
 }

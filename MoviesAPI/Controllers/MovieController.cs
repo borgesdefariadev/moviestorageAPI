3c4ae80507f91413350abceb6c0ca9fccd1b87b4
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-09-12 12:37:41 -0300

    Refactor MovieController to use EF Core context
    Replaced in-memory movie storage with Entity Framework Core MovieContext for persistent data management. Updated controller methods to interact with the database, and cleaned up migration and context files for consistency with EF Core conventions.

--- MoviesAPI/Controllers/MovieController.cs
+++ MoviesAPI/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data;
 using MoviesAPI.Models;
 namespace MoviesAPI.Controllers;
 
@@ -7,27 +8,32 @@ namespace MoviesAPI.Controllers;
 public class MovieController : ControllerBase
 {
 
-    private static List<Movie> movies = new();
-    public static int Id = 1;
+    private MovieContext _context;
+
+    public MovieController(MovieContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public IActionResult AddMovie([FromBody] Movie movie)
     {
         // Implementation for adding a movie
-        movie.Id = Id++;
-        movies.Add(movie);
+        _context.Movies.Add(movie);
+        _context.SaveChanges();
         return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
     }
 
     [HttpGet("{Id}")]
     public IEnumerable<Movie> GetMovies([FromQuery] int skip =0, [FromQuery] int take =10)
     {
-        return movies.Skip(skip).Take(take);
+        return _context.Movies.Skip(skip).Take(take);
     }
 
     public IActionResult GetMovieById(int id)
     {
-        var movie = movies.FirstOrDefault(m => m.Id == id);
+        var movie = _context.Movies.
+            FirstOrDefault(m => m.Id == id);
         if (movie == null) return NotFound();
         return Ok();
 
